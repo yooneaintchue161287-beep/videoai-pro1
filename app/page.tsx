81e48635062617b6faddf18a3a889abd36212132
yooneaintchue161287-beep <yooneaintchue161287@gmail.com>
2026-01-14 14:09:39 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>VideoAI Pro</h1>
+      <p>Landing page coming soon.</p>
+
+      <a href="/generate">Go to Generate Page</a>
+    </main>
+  );
+}
